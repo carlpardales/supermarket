e0853e7fa80b43ab001a75686e2cea70717d6074
Carlito Pardales <carlpardales@gmail.com>
2020-08-28 10:46:58 +1200

    Add peristence layer and Category repository

---
+++ src/Supermarket.API/Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Supermarket.API.Persistence.Repositories;
+using Supermarket.API.Domain.Repositories;
+using Supermarket.API.Persistence.Contexts;
+using Supermarket.API.Domain.Models;
+
+namespace Supermarket.API.Persistence.Repositories
+{
+    public class CategoryRepository : BaseRepository, ICategoryRepository
+    {
+        public CategoryRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Category>> ListAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+    }
+}
